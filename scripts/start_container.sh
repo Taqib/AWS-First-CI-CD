073bda737bf09f0ba98eb238a2ffced97db51d08
Shamsunnabi Taqib <shamsunnabitaqib@gmail.com>
2024-06-25 18:12:30 +0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sreedhar8897/hotel-app:latest
+docker pull taqib4802/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 sreedhar8897/hotel-app
+docker run -dit -p 80:80 taqib4802/hotel-app
 
