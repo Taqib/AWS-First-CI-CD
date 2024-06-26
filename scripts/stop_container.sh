d3e8c86e0e0337bad9db297ce854169f3c3549a9
Shamsunnabi Taqib <shamsunnabitaqib@gmail.com>
2024-06-26 20:20:37 +0600

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-echo "Hi"
+echo "Stopping all running containers..."
+
+# Get the list of all container IDs
+container_ids=$(docker ps -a -q)
+
+# Check if there are any containers to stop
+if [ -n "$container_ids" ]; then
+  docker stop $container_ids
+  echo "All running containers have been stopped."
+else
+  echo "No running containers to stop."
+fi
